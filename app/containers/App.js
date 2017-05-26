2e824cc504f17fe4eeaf6dc73ee4f292ee03d070
AndyXie <andrewcrab.china@gamil.com>
2017-05-26 12:26:46 -0700

    relocate things to folder

---
+++ app/containers/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.refresh();
+  }
+  refresh() {
+    const url = "http://trevor-producer-cdn.api.bbci.co.uk/content/cps/news/world"; // 获取十名用户的信息
+
+    fetch(url)
+      .then((response) => response.json())      // 将返回的response值转换为json，并作为返回值返回
+      .then((data) => this.setState(data))      // data即是上个.then方法的返回值，这里将其打印到console
+      .catch((error) => console.error(error));
+  }
+  render() {
+    return (
+      <View style={[styles.container]}>
+        <Text>{this.state.type}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
